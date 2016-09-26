6700c72a5648a8364de6e157b0de8b84426b3595
Jake Mathews <mathewsj2@wit.edu>
2016-09-26 17:41:57 -0400

    Some uncommited changes I had from a while ago. Have no idea what the changes are

--- modules/time/DigitalClock.js
+++ modules/time/DigitalClock.js
@@ -1,54 +1,54 @@
 //Orginally copied from http://jsfiddle.net/rainev/vx4r5qzv/
 import React from 'react';
 var DigitalClockFace = React.createClass({
-	render: function () {
-		var d = this.props.date;
-		var millis = d.getMilliseconds();
-		var second = d.getSeconds() * 6 + millis * (6 / 1000);
-		var minute = d.getMinutes(); // Get minute number
-		var minuteZero = (minute <= 9) ? "0" : ""; // Add a zero in front of single digit numbers
-		var hour = d.getHours(); // Get hour number;
-		var postm = (hour >= 12) ? "PM" : "AM";
-		// this has to be done after postm calculation
-		hour %= 12; // Convert to 12 12 hour format
-		if (hour === 0) {
-			hour = 12;
-		}
-		return (
-			<div className="digitalClock" style={{ height: 330 * this.props.scale, fontSize: 330 * this.props.scale }}>
-				<div className="digitalHour">{hour}</div>
-				<div className="digitalMinute">{minuteZero + minute}</div>
-				<div className="digitalPostM">{postm}</div>
-			</div>
-		);
-	}
+    render: function () {
+        var d = this.props.date;
+        var millis = d.getMilliseconds();
+        var second = d.getSeconds() * 6 + millis * (6 / 1000);
+        var minute = d.getMinutes(); // Get minute number
+        var minuteZero = (minute <= 9) ? "0" : ""; // Add a zero in front of single digit numbers
+        var hour = d.getHours(); // Get hour number;
+        var postm = (hour >= 12) ? "PM" : "AM";
+        // this has to be done after postm calculation
+        hour %= 12; // Convert to 12 12 hour format
+        if (hour === 0) {
+            hour = 12;
+        }
+        return (
+            <div className="digitalClock" style={{ height: 330 * this.props.scale, fontSize: 330 * this.props.scale }}>
+        		<div className="digitalHour">{hour}</div>
+            	<div className="digitalMinute">{minuteZero + minute}</div>
+            	<div className="digitalPostM">{postm}</div>
+            </div>
+        );
+    }
 });
 
 function transform(str) {
-	return { transform: str };
+    return { transform: str };
 }
 
 function rotate(deg) {
-	return 'rotate(' + deg + 'deg)';
+    return 'rotate(' + deg + 'deg)';
 }
 
 var DigitalClock = React.createClass({
-	getInitialState: function () {
-		return { date: new Date() };
-	},
-	componentDidMount: function () {
-		this.start();
-	},
-	start: function () {
-		var self = this;
-		(function tick() {
-			self.setState({ date: new Date() });
-			requestAnimationFrame(tick);
-		} ());
-	},
-	render: function () {
-		return <DigitalClockFace date={this.state.date} scale={this.props.scale}/>;
-	}
+    getInitialState: function () {
+        return { date: new Date() };
+    },
+    componentDidMount: function () {
+        this.start();
+    },
+    start: function () {
+        var self = this;
+        (function tick() {
+            self.setState({ date: new Date() });
+            requestAnimationFrame(tick);
+        } ());
+    },
+    render: function () {
+        return <DigitalClockFace date={this.state.date} scale={this.props.scale}/>;
+    }
 });
 
 export {DigitalClock};
